556667806ce1d765f2213eef40f9802971ec0ac4
Arthur L. Jones IV <arthur.l.jones@gmail.com>
2013-05-19 01:05:32 -0700

    Add BikeType

---
+++ app/models/bike_type.rb
@@ -0,0 +1,5 @@
+class BikeType < ActiveRecord::Base
+  attr_accessible :name
+
+  validates_presence_of :name
+end
---
+++ db/migrate/20130519080446_create_bike_types.rb
@@ -0,0 +1,9 @@
+class CreateBikeTypes < ActiveRecord::Migration
+  def change
+    create_table :bike_types do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
