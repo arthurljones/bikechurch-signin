c233d5fe5860ece661db90369ab7cabb32a72cf6
Arthur L. Jones IV <arthur.l.jones@gmail.com>
2013-05-18 23:45:30 -0700

    Fix person_id type on ShopOccupant

--- db/migrate/20130518180526_create_shop_occupants.rb
+++ db/migrate/20130518180526_create_shop_occupants.rb
@@ -1,7 +1,7 @@
 class CreateShopOccupants < ActiveRecord::Migration
   def change
     create_table :shop_occupants do |t|
-      t.string :person_id
+      t.integer :person_id
       t.string :type
 
       t.timestamps
